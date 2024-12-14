b07ff94746d645007095a383357d14f784b58b00
Jurand76 <jurand@interia.pl>
2024-12-14 17:06:42 +0100

    flask-cors added, to avoid errors with adding new cars through website

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import sqlite3
 
 # Initlialize Flask app
 app = Flask(__name__)
+CORS(app)
 
 # Database file
 db_file = 'cars.db'
